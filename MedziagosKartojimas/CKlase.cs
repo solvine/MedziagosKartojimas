0da714372711ef496015516c06f67095254bf4f6
Giedre Buga <giedrebuga@gmail.com>
2020-09-21 20:32:14 +0300

    CKlase class added

---
+++ MedziagosKartojimas/CKlase.cs
@@ -0,0 +1,36 @@
+﻿
+namespace MedziagosKartojimas
+{
+    class CKlase : BKlase
+    {
+        public int Skaicius { get; set; }
+        public string Zodis { get; set; }
+        public double DoubleSkaicius { get; set; }
+        public AKlase AObjektas { get; set; }
+
+        public CKlase(string name) : base(name)
+        {
+
+        }
+
+        public CKlase(string name, int skaicius) : base(name)
+        {
+            Skaicius = skaicius;
+        }
+
+        public CKlase(string name, int skaicius, string zodis) : base(name)
+        {
+            Skaicius = skaicius;
+            Zodis = zodis;
+        }
+
+        public CKlase(string name, int skaicius, string zodis, double doubleSkaicius, AKlase aObjektas) : base(name)
+        {
+            Skaicius = skaicius;
+            Zodis = zodis;
+            DoubleSkaicius = doubleSkaicius;
+            AObjektas = aObjektas;
+        }
+
+    }
+}
