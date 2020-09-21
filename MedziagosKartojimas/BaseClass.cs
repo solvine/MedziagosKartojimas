8d71a78a5fd6dae6ed7ed912c17a85c27edab808
Giedre Buga <giedrebuga@gmail.com>
2020-09-21 19:03:01 +0300

    Class BaseClass created

---
+++ MedziagosKartojimas/BaseClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MedziagosKartojimas
+{
+    public abstract class BaseClass
+    {
+        public string Name { get; }
+
+        public BaseClass(string name)
+        {
+            Name = name;
+        }
+    }
+}
