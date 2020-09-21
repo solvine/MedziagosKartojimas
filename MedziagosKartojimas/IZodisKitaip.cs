8f8f662b87fc6c90bbad729e7ffcf76574b37e8f
Giedre Buga <giedrebuga@gmail.com>
2020-09-21 19:37:03 +0300

    Interface IZodisKitaip created

---
+++ MedziagosKartojimas/IZodisKitaip.cs
@@ -0,0 +1,12 @@
+﻿
+namespace MedziagosKartojimas
+{
+    interface IZodisKitaip
+    {
+        string ZodisBeBalsiu();
+        string ZodisBePriebalsiu();
+        string ZodisSuPakeistomisBalsemis(char a);
+        string ZodisSuPakeistomisPriebalsemis(int a);
+
+    }
+}
